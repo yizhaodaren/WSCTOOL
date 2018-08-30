5179e2fd91c5d0ef80a14af6f8e7e189633a5025
wangshuchao <>
2018-08-30 11:22:50 +0800

    初始化

---
+++ WSCTOOL/WSCTOOL/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSCTOOL
+//
+//  Created by 徐天牧 on 2018/8/30.
+//  Copyright © 2018年 WSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSCTOOL/WSCTOOL/Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSCTOOL
+//
+//  Created by 徐天牧 on 2018/8/30.
+//  Copyright © 2018年 WSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
